968f1400204c7105087f58c2131aee2580ff6b08
Victor Breno <victor.brns2@gmail.com>
2020-10-27 17:44:02 -0300

    metade das tarefas concluidas

---
+++ exercicios/bloco_4/[4.3]/tarefa-3.js
@@ -0,0 +1,17 @@
+//Agora inverta o lado do triângulo. 
+
+
+let n = 5;
+let ponto = "*";
+let ladoDoQuadrado = "";
+let vazio = " ";
+
+for (baseVazia = 0; baseVazia < n; baseVazia += 1) {
+    ladoDoQuadrado += vazio
+}
+console.log(ladoDoQuadrado)
+
+
+typeof ladoDoQuadrado
+
+console.log(ladoDoQuadrado.length)
