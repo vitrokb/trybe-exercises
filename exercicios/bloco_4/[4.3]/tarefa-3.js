a41fda2d81943972d6f737916cbf9854c11c239b
Victor Breno <victor.brns2@gmail.com>
2020-10-28 18:30:31 -0300

    adiçao do exercicio 3

--- exercicios/bloco_4/[4.3]/tarefa-3.js
+++ exercicios/bloco_4/[4.3]/tarefa-3.js
@@ -1,17 +1,13 @@
 //Agora inverta o lado do triângulo. 
 
-
-let n = 5;
+// Exercício feito com base no código do Hirayama!!!
+let n = 20;
 let ponto = "*";
 let ladoDoQuadrado = "";
 let vazio = " ";
 
 for (baseVazia = 0; baseVazia < n; baseVazia += 1) {
-    ladoDoQuadrado += vazio
+    ladoDoQuadrado += ponto
 }
+
 console.log(ladoDoQuadrado)
-
-
-typeof ladoDoQuadrado
-
-console.log(ladoDoQuadrado.length)
