0b86e035d0c951cee0c17f8670d60cc24655502b
Victor Breno <victor.brns2@gmail.com>
2020-11-04 18:30:56 -0300

    resolução exercícios

---
+++ exercicios/bloco_5/[5.1]/script.js
@@ -0,0 +1,4 @@
+document.getElementById("paragraph").style.color = "red";
+document.querySelector("#page-title").innerHTML = "Interestelar"; 
+document.getElementsByTagName("p")[1].style.color = "green";
+document.getElementById("subtitle").innerHTML = "Segundo filme favorito é: Inception";
